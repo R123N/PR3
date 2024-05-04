e850f910a5ed939e0be6130899e91bac3c9306de
DebDAB <prrrr116@gmail.com>
2024-05-04 14:43:08 +0400

    del

--- Class1.cs
+++ Class1.cs
@@ -14,7 +14,8 @@ namespace pr3
         { return a - b; }
         public double Multi(double a, double b)
         { return a * b; }
-
+        public double Div(double a, double b)
+        { return a / b; }
     }
 
 }
